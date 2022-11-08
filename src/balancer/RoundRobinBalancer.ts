ed76ab967038a9ccdc30f49a614ffe5604903e09
Robin Thakur <robinttt333@gmail.com>
2022-11-08 09:49:26 +0530

    DI on balancer

--- src/balancer/RoundRobinBalancer.ts
+++ src/balancer/RoundRobinBalancer.ts
@@ -1,9 +1,5 @@
-import path from "path";
-import dotenv from "dotenv";
 import IBalancer from "./interfaces/Ibalancer";
 
-const envPath = path.resolve(__dirname, "..", "..", ".env");
-const config = dotenv.config({ path: envPath });
 /**
  * Class to implement Round Robin balancer
  */
@@ -11,17 +7,19 @@ export default class RoundRobinBalancer implements IBalancer {
   static COLON = ":";
   current: number;
   ports: String[];
+  url: String;
 
-  constructor(ports: String[]) {
+  constructor(ports: String[], url: String) {
     this.ports = ports;
     this.current = 0;
+    this.url = url;
   }
 
   getAddress(): string {
-    const url = [config.parsed!.URL, this.ports[this.current]].join(
+    const address = [this.url, this.ports[this.current]].join(
       RoundRobinBalancer.COLON
     );
     this.current = (this.current + 1) % this.ports.length;
-    return url;
+    return address;
   }
 }
--- src/balancer/routes/post.ts
+++ src/balancer/routes/post.ts
@@ -12,10 +12,13 @@ export default (app: any) => {
   const envPath = path.resolve(__dirname, "..", "..", "..", ".env");
   const config = dotenv.config({ path: envPath });
   const ports = JSON.parse(config.parsed!.PORTS) as unknown as string[];
-  const balancer = new RoundRobinBalancer(ports);
+  const balancer = new RoundRobinBalancer(ports, config.parsed!.URL);
 
   app.post("*", async (req: Request, res: Response) => {
-    const responseFromServer = await axios.post(balancer.getAddress(), req.body);
+    const responseFromServer = await axios.post(
+      balancer.getAddress(),
+      req.body
+    );
     res.send(JSON.stringify(responseFromServer.data));
   });
 };
